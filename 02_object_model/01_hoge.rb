714994eb1117b6ba24e41c332fadf8c63ce0921f
Kentaro Takeyama <n.y.skate.of.minds@gmail.com>
2023-11-26 17:58:46 +0900

    02_object_model 実装

--- 02_object_model/01_hoge.rb
+++ 02_object_model/01_hoge.rb
@@ -5,7 +5,16 @@
 # HogeクラスのスーパークラスはStringである
 # 自身が"hoge"という文字列である時（HogeクラスはStringがスーパークラスなので、当然自身は文字列である）、trueを返すhoge?メソッドが定義されている
 
-class Hoge
+class Hoge < String
+  Hoge = 'hoge'
+
+  def hogehoge
+    Hoge
+  end
+
+  def hoge?
+    self == 'hoge'
+  end
 end
 
 # Q2.
@@ -17,3 +26,38 @@ end
 # - Class
 # - Hash
 # - TrueClass
+class String
+  def hoge
+    Hoge::Hoge
+  end
+end
+
+class Integer
+  def hoge
+    Hoge::Hoge
+  end
+end
+
+class Numeric
+  def hoge
+    Hoge::Hoge
+  end
+end
+
+class Class
+  def hoge
+    Hoge::Hoge
+  end
+end
+
+class Hash
+  def hoge
+    Hoge::Hoge
+  end
+end
+
+class TrueClass
+  def hoge
+    Hoge::Hoge
+  end
+end
