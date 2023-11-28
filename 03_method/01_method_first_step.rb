8e44f5600dd2b911b097f44cb13b6aea38599621
Kentaro Takeyama <n.y.skate.of.minds@gmail.com>
2023-11-28 17:21:42 +0900

    03_method/01_method_first_step

--- 03_method/01_method_first_step.rb
+++ 03_method/01_method_first_step.rb
@@ -8,6 +8,21 @@
 # - 4. `F1.new.respond_to?(定義していないメソッド名)` を実行したときにtrueを返すこと
 
 class F1
+  define_method :hello do
+    'hello'
+  end
+
+  define_singleton_method :world do
+    'world'
+  end
+
+  def method_missing(name)
+    'NoMethodError'
+  end
+
+  def respond_to_missing?(method, include_private = false)
+    true
+  end
 end
 
 # Q2.
@@ -15,4 +30,7 @@ end
 # - 1. 実行するとhiインスタンスメソッドを定義するadd_hiメソッドを定義すること
 
 class F2
+  def add_hi
+    self.class.define_method :hi do; end
+  end
 end
