97f9c0277826c08fe23fbc506b1868769134ab65
Kentaro Takeyama <n.y.skate.of.minds@gmail.com>
2023-12-04 17:51:27 +0900

    04_block/03_simple_bot

--- 04_block/03_simple_bot.rb
+++ 04_block/03_simple_bot.rb
@@ -22,3 +22,27 @@
 # 4. クラスメソッドsettingは、引数を2つ取り、1つ目がキー名、2つ目が設定する値です
 #     1. settingメソッドに渡された値は、クラスメソッド `settings` から返されるオブジェクトに、メソッド名としてアクセスすることで取り出すことができます
 #     2. e.g. クラス内で `setting :name, 'bot'` と実行した場合は、respondメソッドに渡されるブロックのスコープ内で `settings.name` の戻り値は `bot` の文字列になります
+
+class SimpleBot
+  def ask(name)
+    self.class.instance_variable_get(:@hash)[name].call if self.class.instance_variable_get(:@hash)&.key?(name)
+  end
+
+  def self.settings
+    self.class
+  end
+
+  def self.setting(key, value)
+    self.class.define_singleton_method key do
+      value
+    end
+  end
+
+  def self.respond(key, &block)
+    if (hash = instance_variable_get(:@hash))
+      hash[key] = block
+    else
+      instance_variable_set(:@hash, { key => block })
+    end
+  end
+end
