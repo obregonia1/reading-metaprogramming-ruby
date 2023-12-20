9927ea23a74d1e96af13eed44b18dd4ffc1bc1d6
Kentaro Takeyama <n.y.skate.of.minds@gmail.com>
2023-12-20 17:40:18 +0900

    05_class_definition/02_simple_mock

--- 05_class_definition/02_simple_mock.rb
+++ 05_class_definition/02_simple_mock.rb
@@ -37,3 +37,38 @@
 # obj.imitated_method #=> true
 # obj.called_times(:imitated_method) #=> 2
 # ```
+
+module SimpleMock
+  def self.mock(obj)
+    obj.extend(SimpleMock)
+    obj
+  end
+
+  def self.new
+    obj = Object.new
+    mock(obj)
+  end
+
+  def expects(name, value)
+    self.class.define_method name do
+      @counter[name] += 1 if @counter&.key?(name)
+      value
+    end
+    @expects ||= []
+    @expects.push(name.to_sym)
+  end
+
+  def watch(name)
+    (@counter ||= {})[name] = 0
+
+    return if @expects&.include?(name.to_sym)
+
+    define_singleton_method(name) do
+      @counter[name] += 1
+    end
+  end
+
+  def called_times(name)
+    @counter[name]
+  end
+end
